274e19ab43fb0be8cb8477ecca5ee785354c0689
Arkady <prbrq@ya.ru>
2025-08-06 21:13:08 +0300

    Switch to stow

--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-ln -sf "$(pwd)/code/settings.json" ~/.config/Code/User/settings.json
-ln -sf "$(pwd)/code/keybindings.json" ~/.config/Code/User/keybindings.json
-
-bash ./code/install_extensions

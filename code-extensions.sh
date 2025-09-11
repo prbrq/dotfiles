21b4812d27420bccc4ee1de2ec29def76137229d
Arkady <prbrq@ya.ru>
2025-09-11 20:25:41 +0300

    vsc exts added

---
+++ code-extensions.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+code --install-extension codeium.codeium
+code --install-extension editorconfig.editorconfig
+code --install-extension humao.rest-client
+code --install-extension ms-dotnettools.csdevkit
+code --install-extension ms-dotnettools.csharp
+code --install-extension ms-dotnettools.vscode-dotnet-runtime
+code --install-extension streetsidesoftware.code-spell-checker
+code --install-extension streetsidesoftware.code-spell-checker-russian
+code --install-extension usernamehw.errorlens
+code --install-extension vscodevim.vim
